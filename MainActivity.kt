1ae55a42d4b1518912e5b78f620a6497055a6365
haldomae <hal.domae@gmail.com>
2026-06-15 18:41:18 +0900

    MainActivityを検証用に修正

--- MainActivity.kt
+++ MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirebaseMemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // RegisterScreen()
+                    // LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebaseMemoTheme {
-        Greeting("Android")
     }
 }
